645ac8fbbfb78799e6e6ac3d63024b3d31ccd280
Catherine Gloria <catherinegloria@gmail.com>
2020-10-24 14:10:55 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,6 +3,9 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+  
   extend MetaDancing
   include Dance
 
